c96e55e213ef0f91eba915a9be94698f3c83b9a5
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-04-14 09:27:11 +0530

    database added

---
+++ Backend/config/config.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const url = process.env.MONGO_URI;
+    const conn = await mongoose.connect(url, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(
+      `Mongodb DataBase Connected!`
+    );
+  } catch (error) {
+    console.log(`error: ${error.message}`);
+  }
+};
+
+module.exports = connectDB;
