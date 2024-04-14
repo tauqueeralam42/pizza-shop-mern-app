de3426959068372181226369c67a0bbe6e69d5cc
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-04-14 09:27:30 +0530

    server added

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+
+const connectDB = require("./config/config");
+
+//config dotenv
+dotenv.config();
+
+//connection mongodb
+connectDB();
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+
+//route
+app.use("/api/pizzas", require("./routes/pizzaRoute"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/orders", require("./routes/orderRoute"));
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(
+    `Server Running on port no ${port}`
+  );
+});
